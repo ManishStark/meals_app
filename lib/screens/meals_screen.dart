f4116ca1450eac0ef4c93cfcb384b101c6562cd1
ManishStark <mstark825@gmail.com>
2023-06-01 17:52:50 +0530

    First commit

---
+++ lib/screens/meals_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({required this.title, required this.meals, super.key});
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text("No Means Found..!"),
+          const SizedBox(
+            height: 18,
+          ),
+          Text(
+            "Try selecting another category",
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+          )
+        ],
+      ),
+    );
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: (context, index) {
+            return Text(meals[index].title);
+          });
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
+  }
+}
